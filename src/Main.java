98360afc5df5b94806531b0a75bf128d2a78c3e8
kwon183147 <kwonsh1128@gmail.com>
2024-07-12 21:52:45 +0900

    2일차 마무리

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,28 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("프로그램 시작");
+		
 		Scanner sc = new Scanner(System.in);
+		
 		while(true) {
-			System.out.printf("명령어) ");		
+			
+			System.out.printf("명령어) ");	
+			
 			String cmd = sc.nextLine();
-			System.out.println("입력된 명령어: " + cmd );
+			
+			
+			if(cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+					
+			}
+			
 			if(cmd.equals("종료")) {
 				break;
 			}
 		}
+		
 		sc.close();
+		
 		System.out.println("==프로그램 종료==");
 	}
 }
