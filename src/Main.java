e0932c4acb5b305b3e92bdd274f54319af929966
kwon183147 <kwonsh1128@gmail.com>
2024-07-15 21:31:17 +0900

    3차 3교시 article list

--- src/Main.java
+++ src/Main.java
@@ -45,12 +45,26 @@ public class Main {
 				System.out.println(++lastArticleId + "번 글이 생성되었습니다.");
 			
 			} else if(cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다");
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				}
+				
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				}
+
+				System.out.println("번호	|	제목");
+
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+				
 			} else {
 				System.out.println("존재하지않는 명령어 입니다");
 			}
-			
-			
 		}
 		
 		sc.close();
