4a97ae5f4e9dada3f5cdbdf4709a00733ac187b5
kwon183147 <kwonsh1128@gmail.com>
2024-07-19 21:05:25 +0900

    2교시 리팩토리

---
+++ src/com/exam/Bam/Main.java
@@ -0,0 +1,9 @@
+package com.exam.Bam;
+
+import com.exam.Bam.app.App;
+
+public class Main {
+	public static void main(String[] args) {
+		new App().run();
+	}
+}
--- src/com/exam/Bam/util/Util.java
+++ src/com/exam/Bam/util/Util.java
@@ -1,3 +1,4 @@
+package com.exam.Bam.util;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
